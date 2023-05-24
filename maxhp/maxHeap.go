f4f1a349ef9031807568d4b3badf3ae81bcb3bc8
Yen-Kuang-tw <nf8964p5566@gmail.com>
2023-05-25 01:32:40 +0800

    feat: pkg ykheap

--- maxhp/maxHeap.go
+++ maxhp/maxHeap.go
@@ -4,33 +4,28 @@ import (
 	"container/heap"
 )
 
-func InitializeMaxHeap() *maxHeap {
-	hp := &maxHeap{}
-	return hp
-}
-
-type maxHeap []int
+type MaxHeap []int
 
 // Let's build this line for checking whether MaxHeap implements the heap interface
-var _ heap.Interface = (*maxHeap)(nil)
+var _ heap.Interface = (*MaxHeap)(nil)
 
-func (pq maxHeap) Len() int {
+func (pq MaxHeap) Len() int {
 	return len(pq)
 }
 
-func (pq maxHeap) Less(i, j int) bool {
+func (pq MaxHeap) Less(i, j int) bool {
 	return pq[i] > pq[j]
 }
 
-func (pq maxHeap) Swap(i, j int) {
+func (pq MaxHeap) Swap(i, j int) {
 	pq[i], pq[j] = pq[j], pq[i]
 }
 
-func (pq *maxHeap) Push(x interface{}) {
+func (pq *MaxHeap) Push(x interface{}) {
 	*pq = append(*pq, x.(int))
 }
 
-func (pq *maxHeap) Pop() interface{} {
+func (pq *MaxHeap) Pop() interface{} {
 	old := *pq
 	n := len(old)
 	x := old[n-1]
