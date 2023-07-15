b1537a473330742b4cfb313b5683102ae7cca33b
Yen-Kuang-tw <nf8964p5566@gmail.com>
2023-07-15 20:33:18 +0800

    feat: touch .env

--- dataType.go
+++ dataType.go
@@ -1,5 +1,16 @@
 package golib
 
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 type DataType interface {
 	int | int8 | int16 | int32 | int64 | float32 | float64 | byte | string
+}
+
+func GetVersion() string {
+	godotenv.Load("env")
+	return os.Getenv("VERSION")
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/testify v1.8.3 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
